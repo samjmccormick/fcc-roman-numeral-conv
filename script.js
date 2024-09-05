49881ef135d717599570cfa0d4b0ee6dbc291f4f
samjmccormick <samjmccormick@gmail.com>
2024-09-04 20:22:17 -0700

    got it mostly working, just need to create a function that clears the output and input before running the converter

--- script.js
+++ script.js
@@ -1,61 +1,105 @@
 const numberInput = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
-const romanNumerals =[ 
+const romanNumerals = [ 
 {
 	roman: "M",
 	value: 1000
+}, 
+{
+	roman: "CM",
+	value: 900
 }, 
 {
 	roman: "D",
 	value: 500
 }, 
 {
+	roman: "CD",
+	value: 400
+}, 
+{
 	roman: "C",
 	value: 100
+}, 
+{
+	roman: "XC",
+	value: 90
 }, 
 {
 	roman: "L",
 	value: 50
 }, 
 {
+	roman: "XL",
+	value: 40
+}, 
+{
 	roman: "X",
 	value: 10
+}, 
+{
+	roman: "IX",
+	value: 9
 }, 
 {
 	roman: "V",
 	value: 5
 }, 
 {
+	roman: "IV",
+	value: 4
+}, 
+{
 	roman: "I",
 	value: 1
 }
-]
+];
+
 
 const numberToRomanNumeral = (input) => {
-	const array = input.split(input).reverse();
+	if (input === 0) {
+		return "";
+	}
 	
+	for (let i = 0; i < romanNumerals.length; i++) {
+		if(input >= romanNumerals[i].value) {
+			return romanNumerals[i].roman + numberToRomanNumeral(input - romanNumerals[i].value);
+		}
+	} 
 }
 
-console.log(2 * "m");
+console.log(numberToRomanNumeral(4));
+
 
 const checkUserInput = () => {
 	const num = Number(numberInput.value);
 	
 	if (!num || isNaN(num) || !Number.isInteger(num)) {
 		output.classList.toggle("hide");
+		output.classList.toggle("invalid");
 		output.textContent = "Please enter a valid number";
 		return; 
 	} else if (num >= 4000) {
 		output.classList.toggle("hide");
+		output.classList.toggle("invalid");
 		output.textContent = "Please enter a number less than or equal to 3999";
 		return; 
 	} else if (num <= 0) {
 		output.classList.toggle("hide");
+		output.classList.toggle("invalid");
 		output.textContent = "Please enter a number greater than or equal to 1";
 		return; 
-	} 
-	
+	} else {
+		return true
+	}
 }
 
 convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+}
+); 
